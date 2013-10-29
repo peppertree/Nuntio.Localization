5fcd4092cdc29b75ebbe09de78e3cb65c1d17878
peppertree <becker@peppertree-solutions.com>
2013-10-29 13:32:13 +0100

    Initial commit

---
+++ ContentController.vb
@@ -0,0 +1,174 @@
+﻿Imports DotNetNuke.Common.Utilities
+
+Namespace dnnWerk.Libraries.Nuntio.Localization
+
+    Public Class ContentController
+
+        Public _ModuleId As Integer
+        'Public _PortalId As Integer
+        Public _Locale As String
+        Public _ContentList As List(Of ContentInfo)
+
+        Public Sub New(ByVal ModuleId As Integer, ByVal Locale As String, ByVal IncludeUnapproved As Boolean)
+            _ModuleId = ModuleId
+            '_PortalId = Portalid
+            _Locale = Locale
+            If IncludeUnapproved Then
+                _ContentList = GetAllContent()
+            Else
+                _ContentList = GetActiveContent()
+            End If
+        End Sub
+
+        Public Sub UpdateContent(ByVal Key As String, ByVal Content As String, ByVal PortalId As Integer, ByVal ApprovedBy As Integer, ByVal ApprovedDate As DateTime, ByVal CreatedBy As Integer, ByVal CreatedDate As DateTime, ByVal IsApproved As Boolean, ByVal IsOriginal As Boolean, ByVal SourceItemId As Integer)
+
+            Dim objContent As New ContentInfo
+            objContent.ApprovedBy = ApprovedBy
+            objContent.ApprovedDate = ApprovedDate
+            objContent.Content = Content
+            objContent.CreatedBy = CreatedBy
+            objContent.CreatedDate = CreatedDate
+            objContent.IsApproved = IsApproved
+            objContent.IsOriginal = IsOriginal
+            objContent.Key = Key
+            objContent.Locale = _Locale
+            objContent.ModuleId = _ModuleId
+            objContent.PortalId = PortalId
+            objContent.SourceItemId = SourceItemId
+
+            Dim nextVersion As Integer = 1
+
+            Dim objPrevious As ContentInfo = _ContentList.Find(Function(x) (x.Key.ToUpper = Key.ToUpper AndAlso x.SourceItemId = SourceItemId AndAlso x.Locale.ToUpper = _Locale.ToUpper AndAlso x.IsApproved = True))
+            If Not objPrevious Is Nothing Then
+                nextVersion = objPrevious.Version + 1
+            End If
+
+            Dim previousList As List(Of ContentInfo) = GetAllContent().FindAll(Function(x) (x.Key.ToUpper = Key.ToUpper AndAlso x.SourceItemId = SourceItemId AndAlso x.Locale.ToUpper = _Locale.ToUpper))
+            If Not previousList Is Nothing Then
+
+
+                For Each objPreviousContent As ContentInfo In previousList
+
+                    DotNetNuke.Data.DataProvider.Instance.ExecuteNonQuery("pnc_Localization_UpdateLocalizedItem", _
+                                                                                                    objPreviousContent.ItemId, _
+                                                                                                    objPreviousContent.ModuleId, _
+                                                                                                    objPreviousContent.PortalId, _
+                                                                                                    objPreviousContent.SourceItemId, _
+                                                                                                    objPreviousContent.Locale, _
+                                                                                                    objPreviousContent.Key, _
+                                                                                                    objPreviousContent.Content, _
+                                                                                                    False, _
+                                                                                                    objPreviousContent.IsOriginal, _
+                                                                                                    objPreviousContent.CreatedBy, _
+                                                                                                    objPreviousContent.CreatedDate, _
+                                                                                                    objPreviousContent.ApprovedBy, _
+                                                                                                    objPreviousContent.ApprovedDate, _
+                                                                                                    objPreviousContent.Version)
+                Next
+            End If
+
+            DotNetNuke.Data.DataProvider.Instance.ExecuteNonQuery("pnc_Localization_AddLocalizedItem", _
+                                                                            objContent.ModuleId, _
+                                                                            objContent.PortalId, _
+                                                                            objContent.SourceItemId, _
+                                                                            objContent.Locale, _
+                                                                            objContent.Key, _
+                                                                            objContent.Content, _
+                                                                            objContent.IsApproved, _
+                                                                            objContent.IsOriginal, _
+                                                                            objContent.CreatedBy, _
+                                                                            objContent.CreatedDate, _
+                                                                            objContent.ApprovedBy, _
+                                                                            objContent.ApprovedDate, _
+                                                                            nextVersion)
+
+            DataCache.RemoveCache("Nuntio_LocalizedArticles_" & _ModuleId.ToString)
+            DataCache.RemoveCache("Nuntio_LocalizedActiveArticles_" & _ModuleId.ToString)
+
+            _ContentList = GetActiveContent()
+
+
+        End Sub
+
+        Public Sub DeleteContent(ByVal SourceItemId As Integer)
+
+            DotNetNuke.Data.DataProvider.Instance.ExecuteReader("pnc_Localization_DeleteLocalizedItemsBySource", SourceItemId, _ModuleId)
+
+        End Sub
+
+        Public Function GetContentByKey(ByVal SourceItemId As Integer, ByVal Key As String, ByRef IsOriginal As Boolean) As String
+
+            Dim objContent As ContentInfo = _ContentList.Find(Function(x) (x.Key.ToLower = Key.ToLower AndAlso x.SourceItemId = SourceItemId AndAlso x.Locale.ToUpper = _Locale.ToUpper AndAlso x.IsApproved = True))
+
+            If Not objContent Is Nothing Then
+                IsOriginal = objContent.IsOriginal
+                Return objContent.Content
+            End If
+
+            Return ""
+
+        End Function
+
+        Public Function GetActiveContent() As List(Of ContentInfo)
+
+            Dim lst As List(Of ContentInfo) = New List(Of ContentInfo)
+
+            If Not DataCache.GetCache("Nuntio_LocalizedActiveArticles_" & _ModuleId.ToString) Is Nothing Then
+                lst = CType(DataCache.GetCache("Nuntio_LocalizedActiveArticles_" & _ModuleId.ToString), List(Of ContentInfo))
+                Return lst
+            End If
+
+            lst = GetAllContent.FindAll(Function(x) (x.IsApproved = True))
+
+            DataCache.SetCache("Nuntio_LocalizedActiveArticles_" & _ModuleId.ToString, lst)
+
+            Return lst
+
+        End Function
+
+        Public Function GetAllContent() As List(Of ContentInfo)
+
+            Dim lst As List(Of ContentInfo) = New List(Of ContentInfo)
+
+            If Not DataCache.GetCache("Nuntio_LocalizedArticles_" & _ModuleId.ToString) Is Nothing Then
+                lst = CType(DataCache.GetCache("Nuntio_LocalizedArticles_" & _ModuleId.ToString), List(Of ContentInfo))
+                Return lst
+            End If
+
+            lst = CBO.FillCollection(Of ContentInfo)(DotNetNuke.Data.DataProvider.Instance.ExecuteReader("pnc_Localization_GetLocalizedItems", _ModuleId))
+            lst.Sort(Function(x, y) (x.CreatedDate > y.CreatedDate))
+
+            DataCache.SetCache("Nuntio_LocalizedArticles_" & _ModuleId.ToString, lst)
+
+            Return lst
+
+        End Function
+
+        Public Function GetVersions() As List(Of ContentInfo)
+
+            Dim lst As List(Of ContentInfo) = New List(Of ContentInfo)
+            lst = CBO.FillCollection(Of ContentInfo)(DotNetNuke.Data.DataProvider.Instance.ExecuteReader("pnc_Localization_GetLocalizedItems", _ModuleId)).FindAll(Function(y) (y.Key = "TITLE"))
+
+            lst.Sort(Function(x, y) (x.Version > y.Version))
+
+            Dim versions As New List(Of ContentInfo)
+            Dim lastversion As Integer = 0
+
+            For Each objContent As ContentInfo In lst
+                If objContent.Version >= 1 Then
+                    If objContent.Version <> lastversion Then
+                        versions.Add(objContent)                        
+                    End If
+                Else
+                    Exit For
+                End If
+                lastversion = objContent.Version
+            Next
+
+            Return versions
+
+        End Function
+
+    End Class
+
+End Namespace
---
+++ ContentInfo.vb
@@ -0,0 +1,218 @@
+﻿Namespace dnnWerk.Libraries.Nuntio.Localization
+
+    Public Class ContentInfo
+        Implements DotNetNuke.Entities.Modules.IHydratable
+
+        Private _ItemId As Integer
+        Private _content As String
+        Private _key As String
+        Private _ApprovedBy As Integer
+        Private _ApprovedDate As DateTime
+        Private _CreatedBy As Integer
+        Private _CreatedDate As DateTime
+        Private _IsOriginal As Boolean
+        Private _IsApproved As Boolean
+        Private _Locale As String
+        Private _ModuleId As Integer
+        Private _PortalId As Integer
+        Private _SourceItemId As Integer
+        Private _Version As Integer
+
+        Public Property ItemId As Integer
+            Get
+                Return _ItemId
+            End Get
+            Set(ByVal Value As Integer)
+                _ItemId = Value
+            End Set
+        End Property
+
+        Public Property Content As String
+            Get
+                Return _content
+            End Get
+            Set(ByVal Value As String)
+                _content = Value
+            End Set
+        End Property
+
+        Public Property Key As String
+            Get
+                Return _key
+            End Get
+            Set(ByVal Value As String)
+                _key = Value
+            End Set
+        End Property
+
+        Public Property ApprovedBy As Integer
+            Get
+                Return _ApprovedBy
+            End Get
+            Set(ByVal Value As Integer)
+                _ApprovedBy = Value
+            End Set
+        End Property
+
+        Public Property ApprovedDate As DateTime
+            Get
+                Return _ApprovedDate
+            End Get
+            Set(ByVal Value As DateTime)
+                _ApprovedDate = Value
+            End Set
+        End Property
+
+        Public Property CreatedBy As Integer
+            Get
+                Return _CreatedBy
+            End Get
+            Set(ByVal Value As Integer)
+                _CreatedBy = Value
+            End Set
+        End Property
+
+        Public Property CreatedDate As DateTime
+            Get
+                Return _CreatedDate
+            End Get
+            Set(ByVal Value As DateTime)
+                _CreatedDate = Value
+            End Set
+        End Property
+
+        Public Property IsOriginal As Boolean
+            Get
+                Return _IsOriginal
+            End Get
+            Set(ByVal Value As Boolean)
+                _IsOriginal = Value
+            End Set
+        End Property
+
+        Public Property IsApproved As Boolean
+            Get
+                Return _IsApproved
+            End Get
+            Set(ByVal Value As Boolean)
+                _IsApproved = Value
+            End Set
+        End Property
+
+        Public Property Locale As String
+            Get
+                Return _Locale
+            End Get
+            Set(ByVal Value As String)
+                _Locale = Value
+            End Set
+        End Property
+
+        Public Property ModuleId As Integer
+            Get
+                Return _ModuleId
+            End Get
+            Set(ByVal Value As Integer)
+                _ModuleId = Value
+            End Set
+        End Property
+
+        Public Property PortalId As Integer
+            Get
+                Return _PortalId
+            End Get
+            Set(ByVal Value As Integer)
+                _PortalId = Value
+            End Set
+        End Property
+
+        Public Property SourceItemId As Integer
+            Get
+                Return _SourceItemId
+            End Get
+            Set(ByVal Value As Integer)
+                _SourceItemId = Value
+            End Set
+        End Property
+
+        Public Property Version As Integer
+            Get
+                Return _Version
+            End Get
+            Set(ByVal Value As Integer)
+                _Version = Value
+            End Set
+        End Property
+
+        Public Sub Fill(ByVal dr As System.Data.IDataReader) Implements DotNetNuke.Entities.Modules.IHydratable.Fill
+
+            If Not IsDBNull(dr("ItemId")) Then
+                Integer.TryParse(dr("ItemId"), _ItemId)
+            End If
+
+            If Not IsDBNull(dr("ApprovedBy")) Then
+                Integer.TryParse(dr("ApprovedBy"), _ApprovedBy)
+            End If
+
+            If Not IsDBNull(dr("ApprovedDate")) Then
+                Date.TryParse(dr("ApprovedDate"), _ApprovedDate)
+            End If
+
+            If Not IsDBNull(dr("CreatedBy")) Then
+                Integer.TryParse(dr("CreatedBy"), _CreatedBy)
+            End If
+
+            If Not IsDBNull(dr("ModuleId")) Then
+                Integer.TryParse(dr("ModuleId"), _ModuleId)
+            End If
+
+            If Not IsDBNull(dr("PortalId")) Then
+                Integer.TryParse(dr("PortalId"), _PortalId)
+            End If
+
+            If Not IsDBNull(dr("SourceItemId")) Then
+                Integer.TryParse(dr("SourceItemId"), _SourceItemId)
+            End If
+
+            If Not IsDBNull(dr("Version")) Then
+                Integer.TryParse(dr("Version"), _Version)
+            End If
+
+            If Not IsDBNull(dr("CreatedDate")) Then
+                Date.TryParse(dr("CreatedDate"), _CreatedDate)
+            End If
+
+            If Not IsDBNull(dr("Content")) Then
+                _content = Convert.ToString(dr("Content"))
+            End If
+
+            If Not IsDBNull(dr("Key")) Then
+                _key = Convert.ToString(dr("Key"))
+            End If
+
+            If Not IsDBNull(dr("Locale")) Then
+                _Locale = Convert.ToString(dr("Locale"))
+            End If
+
+            If Not IsDBNull(dr("IsApproved")) Then
+                Boolean.TryParse(dr("IsApproved"), _IsApproved)
+            End If
+
+            If Not IsDBNull(dr("IsOriginal")) Then
+                Boolean.TryParse(dr("IsOriginal"), _IsOriginal)
+            End If
+
+        End Sub
+
+        Public Property KeyID As Integer Implements DotNetNuke.Entities.Modules.IHydratable.KeyID
+            Get
+                Return _ItemId
+            End Get
+            Set(ByVal value As Integer)
+                _ItemId = value
+            End Set
+        End Property
+
+    End Class
+
+End Namespace
